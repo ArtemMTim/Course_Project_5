3bce4aa3aa479efcabab4f4dc4e51d7dd92df652
ArtemMTim <artemtimofeev@rambler.ru>
2024-12-12 20:28:04 +0300

    Модель привычки, контроллеры, урлы. Проверка в постмане.

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
+from .models import Habits
+from .serializers import HabitsSerializer
 
-# Create your views here.
+
+class HabitsListApiView(ListAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
+
+    def get_queryset(self):
+        return Habits.objects.filter(is_public=True)
+
+
+class HabitsCreateApiView(CreateAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.creater = self.request.user
+        habit.save()
+
+
+class HabitsRetrieveApiView(ListAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
+
+
+class HabitsUpdateApiView(UpdateAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
+
+
+class HabitsDestroyApiView(DestroyAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
