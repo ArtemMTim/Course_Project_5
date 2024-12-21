be6309e15f2f80b71cc679f639fe910dc798baba
artemtimofeev <artemtimofeev@rambler.ru>
2024-12-21 16:56:58 +0300

    Валидаторы.

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from .validators import action_time, action_periodicity
+from .validators import HabitValidator
 from .models import Habits
 from users.serializers import UserSerializer
 
@@ -10,16 +10,5 @@ class HabitsSerializer(ModelSerializer):
 
     class Meta:
         model = Habits
-        fields = (
-        "creater",
-        "place_to_do",
-        "time_to_do",
-        "action_to_do",
-        "is_pleasant",
-        "related_habit",
-        "periodicity",
-        "reward",
-        "duration",
-        "is_public",
-    )
-        validators = (action_time(field="duration"), action_periodicity(field="periodicity"))
+        fields = '__all__'
+        validators = [HabitValidator(field="__all__")]
--- tracker/validators.py
+++ tracker/validators.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 
-class action_time:
+
+
+class HabitValidator:
+    """Валидатор привычек."""
     def __init__(self, field):
         self.field = field
     def __call__(self, value):
-        time = dict(value).get(self.field)
-        if int(time) > 120:
+        if value['duration'] > 120:
             raise serializers.ValidationError("Время выполнения привычки не может превышать 120 секунд.")
 
-class action_periodicity:
-    def __init__(self, field):
-        self.field = field
-    def __call__(self, value):
-        periodicity = dict(value).get(self.field)
-        if int(periodicity) > 7:
+        if value['periodicity'] > 7:
             raise serializers.ValidationError("Периодичность выполнения привычки не может быть реже раза в неделю.")
+
+        if value["related_habit"] and value["reward"]:
+            raise serializers.ValidationError("Нельзя выбирать связанную привычку и вознаграждение одновременно.")
+
+        if value["is_pleasant"]:
+            if value["reward"] or value["related_habit"]:
+                raise serializers.ValidationError("Нельзя выбирать связанную привычку или вознаграждение для приятной привычки.")
+
+        if not value["related_habit"].is_pleasant:
+            raise serializers.ValidationError(
+                "В связанные привычки можно выбрать только привычки с признаком приятной привычки.")
+
